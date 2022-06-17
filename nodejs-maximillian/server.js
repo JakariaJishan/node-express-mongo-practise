be989a8bf9eb9bd00bdec81a33315f9ad47c4c1b
JakariaJishan <jakariajishan987@gmail.com>
2022-06-17 12:29:07 +0600

    express middleware

--- nodejs-maximillian/server.js
+++ nodejs-maximillian/server.js
@@ -1,7 +1,26 @@
 const http = require("http");
-const requestHandler = require("./routes")
-const server = http.createServer(requestHandler);
 
-server.listen(3000, () => {
-  console.log("listening on port 3000");
-});
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="titile"></input><button type="submit" >add</button></form>')
+})
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use((req, res, next) => {
+  res.send('hello world')
+})
+
+// const requestHandler = require("./routes")
+// const server = http.createServer(requestHandler);
+
+app.listen(3000)
