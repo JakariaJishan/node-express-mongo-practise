01fb59d387cdd48c215c449463813e8466e6b39b
JakariaJishan <jakariajishan987@gmail.com>
2022-06-18 14:57:19 +0600

    ejs done

--- nodejs-maximillian/server.js
+++ nodejs-maximillian/server.js
@@ -9,15 +9,20 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.set('view engine', 'ejs');
 
-app.use("/admin", adminRouter);
+app.use("/admin", adminRouter.router);
 
 app.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "views", "shop.html"));
+  const prod = adminRouter.products;
+  res.render('shop', {
+    prod,
+    shopTitle : 'shop page'
+  })
 });
 
 app.use((req, res, next) => {
-  res.status(404).sendfile(path.join(__dirname, "views", "404.html"))
+  res.status(404).render('404', {title: 'error page'})
 });
 // const requestHandler = require("./routes")
 // const server = http.createServer(requestHandler);
