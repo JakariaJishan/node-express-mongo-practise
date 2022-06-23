7da02d071338219adda4be109920205a8243a553
JakariaJishan <jakariajishan987@gmail.com>
2022-06-23 21:01:46 +0600

    signup done

--- node-express-mongo-sumit/mongoose/routes/userHandler.js
+++ node-express-mongo-sumit/mongoose/routes/userHandler.js
@@ -1,1 +1,28 @@
- 
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const userSchema = require("../schemas/userSchema");
+const User = new mongoose.model("User", userSchema);
+
+router.post("/signup", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      name: req.body.name,
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(200).json({
+      message: "sign up successfull",
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      error: "signup failed",
+    });
+  }
+});
+
+module.exports = router;
