f22f83903de857f3d899cff5f8bde4c5b297057c
JakariaJishan <jakariajishan987@gmail.com>
2022-07-02 08:20:47 +0600

    weather app done

--- nodejs-androw mead/web-server/src/app.js
+++ nodejs-androw mead/web-server/src/app.js
@@ -1,29 +1,57 @@
-const express = require('express');
-const path = require('path');
-const hbs = require('hbs')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
 
 const app = express();
 const port = 3000;
 
 // app.use(express.static(path.join(__dirname, '../public')));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '../template/views'))
-hbs.registerPartials(path.join(__dirname, '../template/partials'))
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../template/views"));
+hbs.registerPartials(path.join(__dirname, "../template/partials"));
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'weather app',
-        name: 'jack'
-    })
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "weather app",
+    name: "jack",
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title:'about',
-        text: 'Welcome to about page'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "about",
+    text: "Welcome to about page",
+  });
+});
 
-app.listen(port, ()=>{
-    console.log('listening to port '+port);
+app.get("/weather", (req, res) => {
+  const location = req.query.location;
+  if (!location) {
+    return res.send({
+      error: " no location specified",
+    });
+  }
+
+  geocode(location, (err, data) => {
+    if (err) {
+      return res.send({ 
+        error: 'invalid location',
+      })
+    }
+    forecast(data.longitude, data.latitude, (error, data) => {
+      if (error) {
+        return console.log(error);
+      }
+      res.send({
+        forecast: data.weather[0].description,
+        location: data.name,
+      });
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log("listening to port " + port);
 });
---
+++ nodejs-androw mead/web-server/src/utils/forecast.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+
+function forecast(long, lat, callback) {
+    // b2dfe0e2aaae8395ca3bef89e495a185
+
+  const uri = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}4&lon=${long}&appid=b2dfe0e2aaae8395ca3bef89e495a185`;
+
+  axios
+    .get(uri)
+    .then((res) => {
+      const result = res.data;
+      callback(undefined, result);
+    // console.log(result.base)
+    })
+    .catch((err) => {
+      callback(err.message, undefined);
+    });
+}
+
+
+module.exports = forecast
